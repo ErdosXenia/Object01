f4c8a88a07e783b713bd44d5505ad3294d4f6437
ErdosXenia <erdos.xenia@gmail.com>
2020-09-15 09:27:33 +0200

    bemutatkozas

--- Object01/Program.cs
+++ Object01/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Object01
 {
+    class ember
+    {
+        public string nev;
+        public int eletkor;
+
+        public string Bemutatkozas()
+        {
+            //string valasz2 = nev + " vagyok" + eletkor + " éves.";
+
+            /*string valasz = $"{nev} vagyok, {eletkor} éves,";
+            return valasz;*/
+
+            return $"{nev} vagyok, {eletkor} éves,";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            ember pisti = new ember();
+            ember eva = new ember();
+
+            eva.nev = "Nagy Éva";
+            eva.eletkor = 18;
+
+            pisti.nev = "Nagy Pisti";
+            pisti.eletkor = 20;
+
+            //pisti.eletkor++;
+
+            Console.WriteLine(pisti.Bemutatkozas());
+            Console.WriteLine(eva.Bemutatkozas());
+
+
+            Console.ReadKey();
         }
     }
 }
